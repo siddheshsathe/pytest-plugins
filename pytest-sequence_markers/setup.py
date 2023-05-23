923d0e436347ff69d709c0b7ebbcee7077f3cb14
Siddhesh Sathe <siddheshsathe@rediffmail.com>
2023-05-23 10:02:13 +0530

    Update setup.py

--- pytest-sequence_markers/setup.py
+++ pytest-sequence_markers/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='pytest_sequence_markers',
-    version='1.0.0',
+    version='0.0.1',
     description="Testing plugin",
     author="Siddhesh Sathe",
-    author_email="temp@gmail.com",
-    license="MIT",
+    author_email="siddheshsathe@rediffmail.com",
+    license="GNU",
     py_modules=['pytest_sequence_markers'],
     requires=['pytest'],
     entry_points={"pytest11": ["pytest_sequence_markers=pytest_sequence_markers",]}
